b51f46919bd70fa0b6b14668f7c7d6d79408996f
Kaze <renyuan7777@gmail.com>
2020-05-30 09:37:06 -0700

    Space Like Movement with Decelaration
    Need additional tuning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 0.001f;
+    public float deceleration = 0.0001f;
     public float hitPoints = 100f;
+    public float maxSpeed = 0.001f;
+    public float horizontalSpeed = 0;
+    public float verticalSpeed = 0;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,18 +20,42 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
 
             // convert user input into world movement
-            float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
-            float verticalMovement = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
-
-            //assign movement to a single vector3
-            Vector3 directionOfMovement = new Vector3(horizontalMovement, verticalMovement, 0);
-
-            // apply movement to player's transform
-            gameObject.transform.Translate(directionOfMovement);
+            horizontalSpeed += Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+            verticalSpeed += Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+            
+            
         }
+        if (horizontalSpeed > maxSpeed)
+        {
+            horizontalSpeed = maxSpeed;
+        }
+        else if (horizontalSpeed < 0 - maxSpeed)
+        {
+            horizontalSpeed = 0 - maxSpeed;
+        }
+        if (verticalSpeed > maxSpeed)
+        {
+            verticalSpeed = maxSpeed;
+        }
+        else if (verticalSpeed < 0 - maxSpeed)
+        {
+            verticalSpeed = 0 - maxSpeed;
+        }
+        horizontalSpeed += (0f - horizontalSpeed) * 0.02f;
+        verticalSpeed += (0f - verticalSpeed) * 0.02f;
+
+        float horizontalMovement = horizontalSpeed;
+        float verticalMovement = verticalSpeed;
+
+        //assign movement to a single vector3
+        Vector3 directionOfMovement = new Vector3(horizontalMovement, verticalMovement, 0);
+
+        // apply movement to player's transform
+        gameObject.transform.Translate(directionOfMovement);
     }
 }
