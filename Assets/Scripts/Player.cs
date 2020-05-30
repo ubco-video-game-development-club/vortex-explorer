5cc69e843525c45680656be0d3b0a28be0443f1c
Kaze <renyuan7777@gmail.com>
2020-05-30 09:15:10 -0700

    Added basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float hitPoints = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+
+            // convert user input into world movement
+            float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+            float verticalMovement = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+            //assign movement to a single vector3
+            Vector3 directionOfMovement = new Vector3(horizontalMovement, verticalMovement, 0);
+
+            // apply movement to player's transform
+            gameObject.transform.Translate(directionOfMovement);
+        }
+    }
+}
