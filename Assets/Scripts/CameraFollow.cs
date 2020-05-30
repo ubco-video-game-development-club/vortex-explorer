a644554145ce77a9d16a28dacdddf9d5663e7b58
Jaden Balogh <jadenbalogh@gmail.com>
2020-05-30 12:49:16 -0700

    Add player and camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Tooltip("Time it takes to move to the player's current position.")]
+    public float followTime;
+
+    private Player player;
+    private Vector2 currentVelocity;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void FixedUpdate()
+    {
+        // we only want to get the player's horizontal movement
+        Vector2 playerHorizontal = player.transform.position * Vector2.right;
+
+        // move the camera towards the player in the horizontal direction
+        Vector3 movement = Vector2.SmoothDamp(transform.position, playerHorizontal, ref currentVelocity, followTime);
+
+        // maintain the camera's current z offset
+        Vector3 zOffset = transform.position.z * Vector3.forward;
+
+        // adjust the camera's current position with the movement from this frame
+        transform.position = movement + zOffset;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 spawnPosition;
+    public float speed;
+
+    private Rigidbody2D rb2D;
+
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        // spawn the player at the spawn position
+        transform.position = spawnPosition;
+
+        // give the player some initial velocity to the right
+        rb2D.AddForce(Vector2.right * speed, ForceMode2D.Impulse);
+    }
+}
