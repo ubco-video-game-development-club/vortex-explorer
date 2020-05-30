dab5ac3bfc0fd298bde48b246b3a7082b7798880
Kaze <renyuan7777@gmail.com>
2020-05-30 11:14:16 -0700

    Switched to using built in unity gravity systems instead of rebuilding.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public double moveSpeed = 0.001;
+    public float moveSpeed = 5f;
     public double deceleration = 0.0001;
-    public double hitPoints = 100;
+    public float hitPoints = 100f;
     public double maxSpeed = 0.001;
     public double horizontalSpeed = 0;
     public double verticalSpeed = 0;
     public int frameCounter = 0;
- 
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("Player::Start cant find RigidBody2D </sadface>");
+        }
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   /*
         frameCounter++;
         if (frameCounter % 3 == 0)
         {
@@ -56,10 +61,31 @@ public class Player : MonoBehaviour
             float verticalMovement = (float)verticalSpeed;
 
             //assign movement to a single vector3
-            Vector3 directionOfMovement = new Vector3(horizontalMovement, verticalMovement, 0);
+            Vector2 directionOfMovement = new Vector2(horizontalMovement, verticalMovement);
 
             // apply movement to player's transform
             gameObject.transform.Translate(directionOfMovement);
+        }
+        */
+    }
+
+    // this is called at a fixed interval for use with physics objects like the RigidBody2D
+    void FixedUpdate()
+    {
+        // check if user has pressed some input keys
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+
+            // convert user input into world movement
+            float horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed;
+            float verticalMovement = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+            //assign world movements to a Veoctor2
+            Vector2 directionOfMovement = new Vector2(horizontalMovement, verticalMovement);
+
+            // apply movement to player's transform
+            rb.AddForce(directionOfMovement);
+            Debug.Log(horizontalMovement);
         }
     }
 }
