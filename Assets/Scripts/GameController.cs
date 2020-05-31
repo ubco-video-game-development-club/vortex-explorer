ec07e37958cd64e18ce1730ad29296e61190c0b1
Kaze <renyuan7777@gmail.com>
2020-05-31 14:16:50 -0700

    Asteroids added, They spawn accoding to mouse location

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,16 +6,22 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     public Vortex vortexPrefab;
+    public Asteroids asteroidsPrefab;
     public float vortexCooldown = 1f;
     public float vortexBuildTime = 1f;
     public float minVortexPower = 1f;
     public float maxVortexPower = 3f;
+    public int asteroidSpawnFrequency = 200;
+    public int asteroidSpawnCounter = 0;
 
     private bool canSpawnVortex;
+    private bool canSpawnAsteroids;
     private float vortexPowerProgress;
+
 
     void Start() {
         canSpawnVortex = true;
+        canSpawnAsteroids = true;
     }
 
     void Update() {
@@ -48,6 +54,14 @@ public class GameController : MonoBehaviour
 
             // reset the power progress
             vortexPowerProgress = 0;
+        }
+
+        //SpawnAsteroids
+        asteroidSpawnCounter++;
+        if (asteroidSpawnCounter % asteroidSpawnFrequency == 0) {
+            //player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+            Asteroids asteroid = Instantiate(asteroidsPrefab, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+            asteroid.transform.position = new Vector3(asteroid.transform.position.x+20, asteroid.transform.position.y, 0); 
         }
     }
 
