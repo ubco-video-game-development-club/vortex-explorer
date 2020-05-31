824d91a02d2ce063efbc9c753f43cb4b96bc0083
Jaden Balogh <jadenbalogh@gmail.com>
2020-05-31 16:55:10 -0700

    Update vortex controls

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,16 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Vortex Settings")]
     public Vortex vortexPrefab;
+    public SpriteRenderer vortexIndicatorPrefab;
+    public LayerMask vortexTargetLayers;
+    [Tooltip("The base radius of a vortex.")]
+    public float vortexBaseRadius = 0.5f;
+    [Tooltip("How much radius does each point of vortex scale add.")]
+    public float vortexRadiusGrowthRate = 0.5f;
+    [Tooltip("The base pull strength of the inner point of a vortex.")]
+    public float vortexBaseInnerStrength = 2f;
+    [Tooltip("The base pull strength of the outer edge of a vortex.")] 
+    public float vortexBaseOuterStrength = 1f;
+    [Tooltip("How much inner pull strength does each point of scale add.")]
+    public float vortexInnerStrengthGrowthRate = 2f;
+    [Tooltip("How much outer pull strength does each point of scale add.")]
+    public float vortexOuterStrengthGrowthRate = 1f;
+    [Tooltip("How long you must wait before placing another vortex.")]
     public float vortexCooldown = 1f;
+    [Tooltip("How long it takes to reach max vortex scale when holding down left-click.")]
     public float vortexBuildTime = 1f;
-    public float minVortexPower = 1f;
-    public float maxVortexPower = 3f;
+    [Tooltip("How much are the vortex values scaled by for the smallest vortex size.")]
+    public float vortexMinScale = 1f;
+    [Tooltip("How much are the vortex values scaled by for the largest vortex size.")]
+    public float vortexMaxScale = 3f;
+    [Tooltip("The force threshold at which objects inside the vortex will be crushed.")]
+    public float vortexCrushForceThreshold = 10f;
+    [Tooltip("The radius threshold at which objects inside the vortex will be crushed.")]
+    public float vortexCrushRadiusThreshold = 2f;
+    [Tooltip("The color of the indicator when the vortex radius is smaller than the crush radius threshold.")]
+    public Color vortexRegularIndicatorColor;
+    [Tooltip("The color of the indicator when the vortex radius is greater than the crush radius threshold.")]
+    public Color vortexCrushIndicatorColor;
 
     private bool canSpawnVortex;
-    private float vortexPowerProgress;
+    private bool isBuildingVortex;
+    private float vortexScaleProgress;
+    private SpriteRenderer vortexIndicator;
 
     void Start() {
+        // spawn the vortex indicator and disable it
+        vortexIndicator = Instantiate(vortexIndicatorPrefab);
+        vortexIndicator.enabled = false;
+
         canSpawnVortex = true;
     }
 
@@ -25,16 +58,47 @@ public class GameController : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
+        // upon pressing the left-click button
+        if (Input.GetButtonDown("Fire1") && canSpawnVortex) {
+            // enable the visual indicator
+            vortexIndicator.enabled = true;
+
+            // start building the vortex scale
+            isBuildingVortex = true;
+        }
+
         // while holding the left-click button
-        if (Input.GetButton("Fire1") && canSpawnVortex) {
+        if (Input.GetButton("Fire1") && canSpawnVortex && isBuildingVortex) {
             // build up the power of the vortex over time
-            vortexPowerProgress = Mathf.Clamp(vortexPowerProgress + Time.deltaTime, 0, vortexBuildTime);
+            vortexScaleProgress = Mathf.Clamp(vortexScaleProgress + Time.deltaTime, 0, vortexBuildTime);
+
+            // get the current mouse position in world coordinates
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // set the indicator at the current mouse position
+            vortexIndicator.transform.position = mousePos;
+
+            // calculate the current radius based on the vortex scale
+            float vortexScale = Mathf.Lerp(vortexMinScale, vortexMaxScale, vortexScaleProgress / vortexBuildTime) - 1;
+            float radius = vortexBaseRadius + vortexRadiusGrowthRate * vortexScale;
+
+            // scale the indicator based on the current radius
+            vortexIndicator.transform.localScale = Vector2.one * radius * 2;
+
+            // set the indicator color based on the current radius
+            vortexIndicator.color = radius > vortexCrushRadiusThreshold ? vortexCrushIndicatorColor : vortexRegularIndicatorColor;
         }
 
         // upon releasing the left-click button
-        if (Input.GetButtonUp("Fire1") && canSpawnVortex) {
+        if (Input.GetButtonUp("Fire1") && canSpawnVortex && isBuildingVortex) {
             // start the vortex spawn cooldown
             StartCoroutine(SpawnVortexCooldown());
+
+            // stop building the vortex scale
+            isBuildingVortex = false;
+
+            // disable the visual indicator
+            vortexIndicator.enabled = false;
 
             // get the current mouse position in world coordinates
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -42,12 +106,31 @@ public class GameController : MonoBehaviour
             // spawn a new vortex object at the mouse position
             Vortex vortex = Instantiate(vortexPrefab, mousePos, Quaternion.identity);
 
-            // activate the vortex at the current power level
-            float powerLevel = Mathf.Lerp(minVortexPower, maxVortexPower, vortexPowerProgress / vortexBuildTime);
-            vortex.Activate(powerLevel);
+            // calculate the vortex stats based on the current vortex scale
+            float vortexScale = Mathf.Lerp(vortexMinScale, vortexMaxScale, vortexScaleProgress / vortexBuildTime) - 1;
+            float radius = vortexBaseRadius + vortexRadiusGrowthRate * vortexScale;
+            float innerStrength = vortexBaseInnerStrength + vortexInnerStrengthGrowthRate * vortexScale;
+            float outerStrength = vortexBaseOuterStrength + vortexOuterStrengthGrowthRate * vortexScale;
 
-            // reset the power progress
-            vortexPowerProgress = 0;
+            Debug.Log(innerStrength + ", " + outerStrength);
+
+            // activate the vortex at the current vortex scale
+            vortex.Activate(radius, innerStrength, outerStrength, vortexCrushForceThreshold, vortexCrushRadiusThreshold, vortexTargetLayers);
+
+            // reset the scale progress
+            vortexScaleProgress = 0;
+        }
+
+        // upon pressing the right-click button
+        if (Input.GetButtonDown("Fire2") && isBuildingVortex) {
+            // stop building the vortex scale
+            isBuildingVortex = false;
+
+            // disable the visual indicator
+            vortexIndicator.enabled = false;
+
+            // reset the scale progress
+            vortexScaleProgress = 0;
         }
     }
 
--- Assets/Scripts/Vortex.cs
+++ Assets/Scripts/Vortex.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class Vortex : MonoBehaviour
 {
-    public float forceThreshold = 0.3f;
-    public float radiusThreshold = 2f;
-    public float baseRadius = 0.5f;
-    public float baseInnerStrength = 2f;
-    public float baseOuterStrength = 1f;
-
     private float radius;
     private float innerStrength;
     private float outerStrength;
+    private float forceThreshold;
+    private float radiusThreshold;
+    private LayerMask targetLayers;
 
     void FixedUpdate() {
         // get all nearby physics objects
-        Collider2D[] targets = Physics2D.OverlapCircleAll(transform.position, radius);
+        Collider2D[] targets = Physics2D.OverlapCircleAll(transform.position, radius, targetLayers);
 
         foreach (Collider2D target in targets) {
             PullTarget pullTarget = target.GetComponent<PullTarget>();
@@ -36,6 +33,8 @@ public class Vortex : MonoBehaviour
 
             // crush the object if it gets too close and the vortex is big enough
             if (pullEffect > forceThreshold && radius > radiusThreshold) {
+                Debug.Log("Oof");
+
                 // if the target is the player
                 Player p;
                 if (pullTarget.TryGetComponent<Player>(out p)) {
@@ -49,11 +48,14 @@ public class Vortex : MonoBehaviour
         }
     }
 
-    public void Activate(float powerLevel) {
-        // scale the base radius and strength values by the power level
-        radius = baseRadius * powerLevel;
-        innerStrength = baseInnerStrength * powerLevel;
-        outerStrength = baseOuterStrength * powerLevel;
+    public void Activate(float radius, float innerStrength, float outerStrength, float forceThreshold, float radiusThreshold, LayerMask targetLayers) {
+        // set the instance variables of this vortex
+        this.radius = radius;
+        this.innerStrength = innerStrength;
+        this.outerStrength = outerStrength;
+        this.forceThreshold = forceThreshold;
+        this.radiusThreshold = radiusThreshold;
+        this.targetLayers = targetLayers;
 
         // set the scale of the gameobject based on its radius
         transform.localScale = Vector2.one * radius * 2;
